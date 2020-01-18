73dadaf9fc5100e9f3d5d1ed62ef04254a3906c8
Terren Culberson <culbersonterren@yahoo.com>
2020-01-18 01:47:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,20 +8,7 @@ attr_accessor :discount, :item, :last_transaction, :total
     @total = 0 
   end 
 
-  def add_item(title,price,quantity = 1) 
-       if quantity>1
-      i=0
-      while i<quantity
-        @items << item
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total 
-  end
+ 
 
   def apply_discount()
     if @discount > 0
