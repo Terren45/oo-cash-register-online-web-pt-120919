fd5120ad770fed621e897b4f365ec5a11bb57127
Terren Culberson <culbersonterren@yahoo.com>
2020-01-18 01:50:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,13 @@ attr_accessor :discount, :item, :last_transaction, :total
   end
 
   def apply_discount()
-    
+     if @discount > 0
+      @to_take_off = (price * discount)/100
+      @total -= @to_take_off
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
   
   def void_last_transaction
     @total -= @price
